9a64f7e20d2fa40e2afce78db6b4ae15c190a756
Gamit Hiren <91384225+HirenGamitDcs22@users.noreply.github.com>
2022-01-12 22:17:08 +0530

    Update Header.js

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,11 +1,11 @@
 import Button from './Button';
 import {useLocation} from 'react-router-dom';
-const Header = ({title,onAdd,showAdd,onLogin,isLogged}) => {
+const Header = ({title,onAdd,showAdd,showLogin,onLogin,isLogged}) => {
     const location=useLocation();
     return (
         <header className='header' style={{color: 'red',backgroundColor: 'black',padding:20}}>
             <h1 >{title}</h1>
-            {!isLogged && <Button color={'blue'} text={'Login'} onClick={onLogin}/>}
+            {!isLogged && <Button color={showLogin?'red':'blue'} text={showLogin?'Close':'Login'} onClick={onLogin}/>}
             {isLogged && location.pathname==="/" &&<Button color={showAdd?'red':'green'} text={showAdd?'Close':'Add'} onClick={onAdd}/>}
         </header>
     )
