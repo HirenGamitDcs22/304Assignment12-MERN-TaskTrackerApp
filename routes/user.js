f63419d9951954ab7def47422841caa3028d1433
Gamit Hiren <91384225+HirenGamitDcs22@users.noreply.github.com>
2022-01-12 21:56:46 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -5,21 +5,33 @@ const userModel=require('../models/user')
 
 
 // Get list of all users
+//GET: users/list
 router.get("/list", async (req, res) => {
   const userList = await userModel.find({});
-
   if (userList.length === 0) {
-    return res.json({ data: "no users in fullstack" });
+    return res.json({ data: "no users in fullstack", });
   }
-
   return res.json({ data: userList });
 });
 
 // Register user
+//POST: users/registration
 router.post("/registration", async(req, res) => {
   const newUser  = req.body;
   await userModel.create(newUser);
   return res.json({ msg: "registered successfully",data:newUser });
 });
 
+//Login
+//POST: /users/login
+router.post("/login",async(req,res)=>{
+  const uname=req.body.uname;
+  const pwd=req.body.pwd;
+  const user=await userModel.find({ username:uname, password:pwd });
+  if(user.length === 0){
+      return  res.json({data:"User is not registered",code:'NOT_REGISTERED'});
+  }else{
+      return res.json({data:"Login successfully",code:'SUCCESS'});
+  }
+});
 module.exports=router
