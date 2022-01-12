fb9f87557cad9c07f388dc6f65fb9919cfa95119
Gamit Hiren <91384225+HirenGamitDcs22@users.noreply.github.com>
2022-01-12 22:06:30 +0530

    Update tasks.js

--- routes/tasks.js
+++ routes/tasks.js
@@ -2,11 +2,13 @@ const express=require('express')
 const router=express.Router();
 
 const tasksModel=require('../models/tasks')
+
 //Fetch Tasks 
+//GET: /tasks/list
 router.get("/list",async(req,res)=>{
   const taskList = await tasksModel.find();
   if (taskList.length === 0) {
-    return res.json({ data: "no Tasks in Task Tracker" });
+    return res.json({code:'NO_TASKS' });
   }
   return res.json({ data: taskList });
 })
@@ -16,35 +18,54 @@ router.get("/fetchtask/:id",async(req,res)=>{
   const id=req.params.id;
   const taskData=await tasksModel.findById(id)
   if(taskData.length === 0){
-    return res.json({msg:`Task is not found for id ${id}`})
+    return res.json({msg:`Task is not found for id ${id}`,code:'NO_TASK_FOUND'})
   }
   return res.json({data:taskData})
 })
 
 //Add Task
+//POST: /tasks/addtask
 router.post("/addtask",async(req,res)=>{
     const newTask = req.body;
     tasksModel.create(newTask);
     return res.json({ data: newTask });
 })
-//Update Task
 
-//PUT: /tasks/updatereminder/:id
+//Update Task
+//PUT: /tasks/updatetask/1
+router.put("/updatetasks/:id",async(req,res)=>{
+  const id=req.params.id;
+  const text=req.body.text
+  const day=req.body.day
+  const reminder=req.body.reminder;
+  const updatedData=await tasksModel.findOneAndUpdate(
+      {_id:id},
+      {text:text},
+      {day:day},
+      {reminder: reminder},
+      {new:true}
+  );
+  return res.json({data:updatedData});
+});
+
+//PUT: /tasks/updatereminder/1
 // Update Reminder
 router.put("/updatereminder/:id",async(req,res)=>{
   const id=req.params.id;
-  const updatedData=await productModel.findOneAndUpdate(
-      {id:id},
-      {reminder:!reminder},
+  const reminder=req.body.reminder;
+  const updatedData=await tasksModel.findOneAndUpdate(
+      {_id:id},
+      {reminder: reminder},
       {new:true}
   );
-  return res.json({"Upadated Record Is ":updatedData});
+  return res.json({data:updatedData});
 });
 
 //Delete Task
+//DELETE: /tasks/deltask/1
 router.delete("/deltask/:id",async(req,res)=>{
   const id=req.params.id;
-  const deldata=await tasksModel.findOneAndDelete({id:id});
+  const deldata=await tasksModel.findOneAndDelete({_id:id});
   return res.json({"Deleted Data Is ":deldata});
 })
 module.exports=router
