8e1c0540b9ba7af85a897f38773636aa94c9251e
Gamit Hiren <91384225+HirenGamitDcs22@users.noreply.github.com>
2022-01-12 22:07:27 +0530

    Update index.js

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,11 +1,9 @@
 import { combineReducers } from "redux";
 import loggedReducer from "./isLogged";
 import taskReducer from "./tasks"
-import userReducer from "./user";
 
 const allReducers = combineReducers({
     isLogged:loggedReducer,
-    tasks:taskReducer,
-    user:userReducer
+    tasks:taskReducer
 })
 export default allReducers
