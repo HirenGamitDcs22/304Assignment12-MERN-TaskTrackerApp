c24b6eb8f470d1c5e8950ef7b4a5e18d3fe7e716
Gamit Hiren <91384225+HirenGamitDcs22@users.noreply.github.com>
2022-01-17 14:55:08 +0530

    Update Task.js

--- client/src/components/Task.js
+++ client/src/components/Task.js
@@ -1,13 +1,23 @@
-import {FaTimes} from 'react-icons/fa'
-const Task=({task, onDelete, onToggle})=> {
+import {FaTimes,FaEdit} from 'react-icons/fa'
+const Task=({task, onDelete, onToggle,onUpdate})=> {
     return (
         <div className={`task ${task.reminder ? 'reminder':''}`}  
-        onDoubleClick={() => onToggle(task.id)}>
+        onDoubleClick={() => onToggle(task._id)}>
             <h3>
-                {task.text} <FaTimes
-                 style={{color:'red',cursor:'pointer'}}
-                 onClick={()=>onDelete(task.id)}/></h3>
+                {task.text}
+                <h4> 
+                <FaEdit 
+                    style={{color:'blue',cursor:'pointer'}}
+                    onClick={()=>onUpdate(task._id)}
+                 />
+                 <FaTimes
+                    style={{color:'red',cursor:'pointer'}}
+                    onClick={()=>onDelete(task._id)}/> </h4>
+            </h3>
             <p>{task.day}</p>
+            <h3>
+                
+            </h3>
         </div>
     )
 }
