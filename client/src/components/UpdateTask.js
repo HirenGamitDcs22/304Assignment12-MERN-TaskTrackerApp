3f9138db2141efa9aa59338b4180f2a611332b2b
Gamit Hiren <91384225+HirenGamitDcs22@users.noreply.github.com>
2022-01-19 16:49:42 +0530

    Create UpdateTask.js

---
+++ client/src/components/UpdateTask.js
@@ -0,0 +1,44 @@
+import {useEffect, useState} from 'react'
+
+function UpdateTask({onUpdate,title,updatetask}) {
+    const [id,setId]=useState('')
+    const [text,setText]=useState('')
+    const [day,setDay]=useState('')
+    const [reminder,setReminder]=useState(false)
+    console.log("Hello Update Hiren")
+    useEffect(()=>{
+        console.log(updatetask._id)
+        setId(updatetask._id)
+        setText(updatetask.text)
+        setDay(updatetask.day)
+        setReminder(updatetask.reminder)
+    })
+    console.log("Hello Update")
+    const onSubmit=(e)=>{
+        e.preventDefault()
+        onUpdate({text,day,reminder})
+        setText('')
+        setDay('')
+        setReminder(false)
+    }
+    return (
+        <form className='add-form' onSubmit={onSubmit}>
+            <div className='form-control'>
+                <label>Task</label>
+                <input type='text' placeholder='Add Task'
+                value={text} onChange={(e)=>setText(e.target.value)}></input>
+            </div>
+            <div className='form-control'>
+                <label>Day</label>
+                <input type='text' placeholder='Add Date & Time' value={day} onChange={(e)=>setDay(e.target.value)}></input>
+            </div>
+            <div className='form-control form-control-check'>
+                <label>Set Reminder</label>
+                <input type='checkbox' checked={reminder} value={reminder} onChange={(e) =>setReminder(e.currentTarget.checked)}></input>
+            </div>
+            <input type='submit' value={title} className='btn btn-block' ></input>
+        </form>
+    )
+}
+
+export default UpdateTask
