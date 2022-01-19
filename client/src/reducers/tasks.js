daa574ad2ea2b91c46255a75d7a10103d9f0a116
Gamit Hiren <91384225+HirenGamitDcs22@users.noreply.github.com>
2022-01-19 16:51:37 +0530

    Update tasks.js

--- client/src/reducers/tasks.js
+++ client/src/reducers/tasks.js
@@ -1,18 +1,18 @@
-const taskReducer=(tasks=[],{type,payload})=>{
-    switch (type) {
-        case "ADD_TASK":
-            return [{...tasks,payload}]
+const taskReducer=(tasks=[],action)=>{
+    switch (action.type) {
         case "GET_TASKS":
-            return payload
+            return action.payload
+        case "ADD_TASK":
+            const {task} = action
+            return[...tasks,{...task}]
         case "DELETE_TASKS":
-                return tasks.data.filter(t=>t._id != payload)
-        case "TOGGLE_REMIND":
-            return tasks
+            return tasks.filter(t=>t._id !== action.id)
+        case "TOGGLE_REMIND":  
+            return tasks.map(task=>task._id===action.id ? {...task, reminder:!task.reminder} : task)
         case "FETCH_TASK":
             return tasks
-
         default:
             return tasks
-    }
+    }  
 }
 export default taskReducer
